cc8a263b37446948e62108011b61ae97cf51b975
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-04-02 19:35:25 +0800

    helper数据获取

--- miniprogram/pages/main/main.js
+++ miniprogram/pages/main/main.js
@@ -6,26 +6,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-    college: ['不限','计算机科学与工程学院、软件学院、人工智能学院', '中国', '巴西', '日本'],
+    collegeMap:{},
+
+    college: ['不限'],
     collegeDisp:'不限',
     collegeIndex: 0,
+
+    majority: ['不限'],
+    majorityDisp:'不限',
+    majorityIndex:0,
 
     gander: ['不限','男', '女'],
     ganderDisp:'不限',
     ganderIndex: 0,
 
-    year: ['不限','2017', '2016','2015','2014'],
-    yearDisp:'不限',
-    yearIndex: 0,
+    entryDate: ['不限'],
+    entryDisp: '不限',
+    entryIndex: 0,
 
+    graduationDate: ['不限'],
+    graduationDateDisp: '不限',
+    graduationDateIndex: 0,
 
+    name: '',
+    schoolnum: ''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: async function (options) {
+    // 获取辅助数据
+    wx.showLoading({title:'更新辅助数据'})
+    let res = await wx.cloud.callFunction({
+      name:'getHelperData'
+    })
+    this.setData({
+      collegeMap:res.result.collegeMap,
+      entryDate:['不限', ...res.result.entryDate],
+      graduationDate:['不限', ...res.result.graduationDate],
+      college:['不限', ...Object.keys(res.result.collegeMap)]
+    })
+    wx.hideLoading()
   },
 
   /**
@@ -79,11 +101,24 @@ Page({
 
   bindPickerCollege: function (e) {
     //console.log('picker发送选择改变，携带值为', e.detail.value)
+    console.log(e.detail.value)
     this.setData({
       collegeIndex: e.detail.value,
-      collegeDisp: this.data.college[e.detail.value].slice(0, 12) + (this.data.college[e.detail.value].length>12? '...' :'')
+      collegeDisp: this.data.college[e.detail.value].slice(0, 12) + (this.data.college[e.detail.value].length>12? '...' :''),
+      majority: e.detail.value != 0 ? ['不限', ...Object.keys(this.data.collegeMap[this.data.college[e.detail.value]])]:['不限'],
+      majorityIndex: 0,
+      majorityDisp: '不限'
     })
   },
+
+  bindPickerMajority: function (e) {
+    //console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      majorityIndex: e.detail.value,
+      majorityDisp: this.data.majority[e.detail.value].slice(0, 12) + (this.data.majority[e.detail.value].length>12? '...' :'')
+    })
+  },
+
   bindPickerGander: function(e){
     this.setData({
       ganderIndex:e.detail.value,
@@ -95,6 +130,12 @@ Page({
     this.setData({
       yearIndex:e.detail.value,
       yearDisp:this.data.year[e.detail.value]
+    })
+  },
+
+  bindName: function(e){
+    this.setData({
+      name:e.detail.value
     })
   }
 
